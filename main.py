e57ceb1fd523825fc1975ce1613c6f74aefdf3b7
Michael Salvatore <msalvatore22@gmail.com>
2021-10-19 22:06:35 -0400

    draw grid fixed

--- main.py
+++ main.py
@@ -173,12 +173,14 @@ def draw_text_middle(text, size, color, surface):
     pass
    
 def draw_grid(surface, grid, row, col):
+    sx = top_left_x
+    sy = top_left_y
 
     for i in range(len(grid)):
+        pygame.draw.line(surface, (128, 128, 128), (sx, sy+ i*block_size), (sx+play_width, sy+ i*block_size))
         for j in range(len(grid[i])):
-            pygame.draw.rect(surface, grid[i][j], (top_left_x + j*block_size, top_left_y+ i*block_size, block_size, block_size), 0)
-    
-    pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 0)
+            pygame.draw.line(surface, (128, 128, 128), (sx + j*block_size, sy), (sx+ j*block_size, sy+ play_height))
+
     
  
 def clear_rows(grid, locked):
@@ -195,6 +197,13 @@ def draw_window(surface, grid):
     label = font.render('Tetris', 1, (255, 2555, 255))
 
     surface.blit(label, (top_left_x + play_width/2 - (label.get_width()/2)), 30)
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j], (top_left_x + j*block_size, top_left_y+ i*block_size, block_size, block_size), 0)
+    
+    pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 0)
+
     draw_grid(surface, grid)
     pygame.display.update()
  
