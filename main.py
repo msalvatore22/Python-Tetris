0a847f5f78bb7b862484800c8ca733e2dd2d2df6
Michael Salvatore <msalvatore22@gmail.com>
2021-10-20 21:13:32 -0400

    Delete rows working

--- main.py
+++ main.py
@@ -212,10 +212,44 @@ def draw_grid(surface, row, col):
 
  
 def clear_rows(grid, locked):
-    pass
+    # need to see if row is clear the shift every other row above down one
+    inc = 0
+    for i in range(len(grid)-1,-1,-1):
+        row = grid[i]
+        if (0, 0, 0) not in row:
+            inc += 1
+            # add positions to remove from locked
+            ind = i
+            for j in range(len(row)):
+                try:
+                    del locked[(j, i)]
+                except:
+                    continue
+    if inc > 0:
+        for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
+            x, y = key
+            if y < ind:
+                newKey = (x, y + inc)
+                locked[newKey] = locked.pop(key)
+
  
 def draw_next_shape(shape, surface):
-    pass
+    font = pygame.font.SysFont('comicsans', 30)
+    label = font.render('Next Shape', 1, (255,255,255))
+
+    sx = top_left_x + play_width + 50
+    sy = top_left_y + play_height/2 - 100
+
+    format = shape.shape[shape.rotation % len(shape.shape)]
+    
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                pygame.draw.rect(surface, shape.color, (sx + j*block_size, sy+i*block_size, block_size, block_size), 0)
+
+    surface.blit(label, (sx + 10, sy - 50))
+
  
 def draw_window(surface, grid):
     surface.fill((0,0,0))
@@ -232,7 +266,7 @@ def draw_window(surface, grid):
     # draw grid and border
     draw_grid(surface, 20, 10)
     pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 5)
-    pygame.display.update()
+    # pygame.display.update()
  
 def main(win):
     locked_positions = {}
@@ -295,8 +329,12 @@ def main(win):
             current_piece = next_piece
             next_piece = get_shape()
             change_piece = False
+            clear_rows(grid, locked_positions)
+        
         
         draw_window(win, grid)
+        draw_next_shape(next_piece, win)
+        pygame.display.update()
         if check_lost(locked_positions):
             run = False
     
